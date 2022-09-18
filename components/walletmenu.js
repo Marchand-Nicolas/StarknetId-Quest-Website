3ec3dfe7d2c57451a42405a7637d71981ea6db19
nicolasito1411 <60229704+Marchand-Nicolas@users.noreply.github.com>
2022-09-18 15:30:22 +0200

    Improving wallet menu

--- components/walletmenu.js
+++ components/walletmenu.js
@@ -24,9 +24,11 @@ function WalletMenu({ close }) {
             <p className={styles.menu_title}>{connectors.length ? "Please select a wallet" : "You need a Starknet wallet"}</p>
             {
                 connectors.length === 0 ?
-                <a className={styles.button} href="https://chrome.google.com/webstore/detail/argent-x-starknet-wallet/dlcobpjiigpikoobohmabehhmhfoodbb" target="_blank" rel="noreferrer" >
-                    <svg className={styles.button_icon} fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"></path></svg>
-                    <p className={styles.button_text}>Install Argent X Wallet on Chrome</p>
+                <a className={["button gold big", styles.button].join(" ")} href="https://chrome.google.com/webstore/detail/argent-x-starknet-wallet/dlcobpjiigpikoobohmabehhmhfoodbb" target="_blank" rel="noreferrer">
+                    <div className='line'>
+                        <svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"></path></svg>
+                        <p>Install Argent X Wallet on Chrome</p>
+                    </div>
                 </a> :
                 connectors.map((connector, index) => 
                     <button onClick={() => {
