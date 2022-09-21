8b2cdf8ff44f9692af24e461b36b7ba902b7fbd7
nicolasito1411 <60229704+Marchand-Nicolas@users.noreply.github.com>
2022-09-21 19:02:13 +0200

    Adding quizz

--- utils/questList.js
+++ utils/questList.js
@@ -23,76 +23,86 @@ export default [{
             icon: <svg className={styles.quest_point_icon} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
             <path strokeLinecap="round" strokeLinejoin="round" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
             </svg>,
-            steps: [3, 3, 3, 3, 4],
+            steps: [3, 3, 3, 3, 3, 3, 4],
             questions : [
                 {
-                    name: "How big is the map of Eykar ?",
+                    name: "What is starknet.id ?",
                     choices: [
                         {
-                            name: "Small"
+                            name: "A simple naming service"
                         },
                         {
-                            name: "Medium"
-                        },
-                        {
-                            name: "Large"
-                        },
-                        {
-                            name: "Extra Large"
-                        },
-                        {
-                            name: "Bigger than our universe",
+                            name: "An all in one Identity Service",
                             correct: true
                         },
+                    ]
+                },
+                {
+                    name: "Why is it useful for users to verify their social media on their starknet.id ?",
+                    choices: [
                         {
-                            name: "Infinite"
+                            name: "To permit every starknet protocols to give you reward based on your on and off chain activity",
+                        },
+                        {
+                            name: "To collect your data"
+                        },
+                        {
+                            name: "To link your social accounts with your public address",
+                            correct: true
                         }
                     ]
                 },
                 {
-                    name: "Can we make money playing the game?",
+                    name: "Can we create a .stark subdomain with starknet.id (example: vitalik.ethereum.stark) ?",
                     choices: [
                         {
-                            name: "Yes",
-                            correct: true
+                            name: "Yes we can create max 10 subdomains per domain"
                         },
                         {
-                            name: "No"
+                            name: "No we can’t"
+                        },
+                        {
+                            name: "Yes we can create unlimited subdomains",
+                            correct: true
                         }
                     ]
                 },
                 {
-                    name: "How to earn real money playing the game?",
+                    name: "What is the utility of setting a domain as your main domain ?",
                     choices: [
                         {
-                            name: "By building diamond mines"
-                        },
-                        {
-                            name: "By earning Eykar tokens"
-                        },
-                        {
-                            name: "By having the most prestigious Guild.",
+                            name: "Because your address will be linked with your main domain and therefore protocols will be able to see that you own a .stark",
                             correct: true
                         },
                         {
-                            name: "By attacking other players to steal the money they have invested"
-                        }
+                            name: "It’s useless"
+                        },
+                        {
+                            name: "It permits to link several domains to one starknet.id"
+                        },
                     ]
                 },
                 {
-                    name: "How can you participate in the creation of the game?",
-                    multiple: true,
+                    name: "Will the testnet .stark name be useful on mainnet",
                     choices: [
                         {
-                            name: "By participating in the development, all the code is open source on Github",
-                            correct: true
+                            name: "Yes"
                         },
                         {
-                            name: "By making suggestions (on the Discord server for example)",
+                            name: "No",
                             correct: true
                         },
+                    ]
+                },
+                {
+                    name: "Is it possible to have several .stark domains linked to one identity ?",
+                    choices: [
                         {
-                            name: "By inviting lots of bots to the Discord server 😡😠😠"
+                            name: "Yes"
+                        },
+                        {
+                            name: "No",
+                            correct: true
                         },
                     ]
                 },
@@ -127,45 +137,47 @@ export default [{
                                         ]
                                     }
                                 },
-                        }
-                    ]
-                },
-                {
-                    name: "Get a domain",
-                    id: 12,
-                    description: <>Buy a .stark domain on <a className='link' href='https://starknet.id/' target="_blank" rel="noreferrer">Starknet.id</a></>,
-                    icon: <svg className={styles.quest_point_icon} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M12.75 3.03v.568c0 .334.148.65.405.864l1.068.89c.442.369.535 1.01.216 1.49l-.51.766a2.25 2.25 0 01-1.161.886l-.143.048a1.107 1.107 0 00-.57 1.664c.369.555.169 1.307-.427 1.605L9 13.125l.423 1.059a.956.956 0 01-1.652.928l-.679-.906a1.125 1.125 0 00-1.906.172L4.5 15.75l-.612.153M12.75 3.031a9 9 0 00-8.862 12.872M12.75 3.031a9 9 0 016.69 14.036m0 0l-.177-.529A2.25 2.25 0 0017.128 15H16.5l-.324-.324a1.453 1.453 0 00-2.328.377l-.036.073a1.586 1.586 0 01-.982.816l-.99.282c-.55.157-.894.702-.8 1.267l.073.438c.08.474.49.821.97.821.846 0 1.598.542 1.865 1.345l.215.643m5.276-3.67a9.012 9.012 0 01-5.276 3.67m0 0a9 9 0 01-10.275-4.835M15.75 9c0 .896-.393 1.7-1.016 2.25" />
-                    </svg>
-                    ,
-                    steps: [6, 4],
-                    customContent: [
-                        <p>
-                            Buy a .stark domain on <a className='link' href="https://starknet.id/" target="_blank" rel="noreferrer">Starknet.id</a>, then click Okay
-                        </p>,
-                    ],
-                        connected: [
-                        {
-                            name: "Main domain",
-                                id: 14,
-                                description: "Set you domain as main domain",
-                                icon: <svg className={styles.quest_point_icon} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-                                </svg>,
-                                steps: [6, 4],
-                                details: {
-                                    0: {
-                                        buttons: [
-                                            <a href="https://starknet.id" target="_blank" rel="noreferrer"><button className="button gold">Open Starknet.id</button></a>
+                                connected: [
+                                    {
+                                        name: "Get a domain",
+                                        id: 12,
+                                        description: <>Buy a .stark domain on <a className='link' href='https://starknet.id/' target="_blank" rel="noreferrer">Starknet.id</a></>,
+                                        icon: <svg className={styles.quest_point_icon} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M12.75 3.03v.568c0 .334.148.65.405.864l1.068.89c.442.369.535 1.01.216 1.49l-.51.766a2.25 2.25 0 01-1.161.886l-.143.048a1.107 1.107 0 00-.57 1.664c.369.555.169 1.307-.427 1.605L9 13.125l.423 1.059a.956.956 0 01-1.652.928l-.679-.906a1.125 1.125 0 00-1.906.172L4.5 15.75l-.612.153M12.75 3.031a9 9 0 00-8.862 12.872M12.75 3.031a9 9 0 016.69 14.036m0 0l-.177-.529A2.25 2.25 0 0017.128 15H16.5l-.324-.324a1.453 1.453 0 00-2.328.377l-.036.073a1.586 1.586 0 01-.982.816l-.99.282c-.55.157-.894.702-.8 1.267l.073.438c.08.474.49.821.97.821.846 0 1.598.542 1.865 1.345l.215.643m5.276-3.67a9.012 9.012 0 01-5.276 3.67m0 0a9 9 0 01-10.275-4.835M15.75 9c0 .896-.393 1.7-1.016 2.25" />
+                                        </svg>
+                                        ,
+                                        steps: [6, 4],
+                                        customContent: [
+                                            <p>
+                                                Buy a .stark domain on <a className='link' href="https://starknet.id/" target="_blank" rel="noreferrer">Starknet.id</a>, then click Okay
+                                            </p>,
+                                        ],
+                                            connected: [
+                                            {
+                                                name: "Main domain",
+                                                    id: 14,
+                                                    description: "Set you domain as main domain",
+                                                    icon: <svg className={styles.quest_point_icon} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
+                                                        <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+                                                    </svg>,
+                                                    steps: [6, 4],
+                                                    details: {
+                                                        0: {
+                                                            buttons: [
+                                                                <a href="https://starknet.id" target="_blank" rel="noreferrer"><button className="button gold">Open Starknet.id</button></a>
+                                                            ]
+                                                        }
+                                                    },
+                                                    customContent: [
+                                                        <p>
+                                                            Set your .stark domain as main domain on <a className='link' href="https://starknet.id/" target="_blank" rel="noreferrer">Starknet.id</a>, then click Okay
+                                                        </p>,
+                                                    ],
+                                            }
                                         ]
                                     }
-                                },
-                                customContent: [
-                                    <p>
-                                        Set your .stark domain as main domain on <a className='link' href="https://starknet.id/" target="_blank" rel="noreferrer">Starknet.id</a>, then click Okay
-                                    </p>,
-                                ],
-                        }
+                                ]
+                        },
                     ]
                 }
             ]
