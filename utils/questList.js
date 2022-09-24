fea3bcaa0021f35f86f0467f58c7f2a400f98048
nicolasito1411 <60229704+Marchand-Nicolas@users.noreply.github.com>
2022-09-24 19:10:53 +0200

    Improving quest tree

--- utils/questList.js
+++ utils/questList.js
@@ -145,27 +145,6 @@ export default function getQuestList(datas) {
                             },
                             connected: [
                                 {
-                                    name: "Avatar",
-                                    id: 3,
-                                    description: "Change your twitter profile pic by your identity avatar",
-                                    icon: <svg className={styles.quest_point_icon} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M9.53 16.122a3 3 0 00-5.78 1.128 2.25 2.25 0 01-2.4 2.245 4.5 4.5 0 008.4-2.245c0-.399-.078-.78-.22-1.128zm0 0a15.998 15.998 0 003.388-1.62m-5.043-.025a15.994 15.994 0 011.622-3.395m3.42 3.42a15.995 15.995 0 004.764-4.648l3.876-5.814a1.151 1.151 0 00-1.597-1.597L14.146 6.32a15.996 15.996 0 00-4.649 4.763m3.42 3.42a6.776 6.776 0 00-3.42-3.42" />
-                                    </svg>,
-                                    steps: [6, 4],
-                                    customContent: [
-                                        <p>
-                                            Change your twitter profile pic by your identity avatar. Don't try to cheat, or you won't get the final reward 🤨
-                                        </p>,
-                                    ],
-                                    details: {
-                                        0: {
-                                            buttons: [
-                                                <a href={"https://app.starknet.id/identities/" + datas.identityTokenId + "/"} target="_blank" rel="noreferrer"><button className="button gold">Open Starknet.id</button></a>
-                                            ]
-                                        }
-                                    },
-                                },
-                                {
                                     name: "Get a domain",
                                     id: 12,
                                     description: <>Buy a .stark domain on <a className='link' href='https://starknet.id/' target="_blank" rel="noreferrer">Starknet.id</a></>,
@@ -216,7 +195,28 @@ export default function getQuestList(datas) {
                                                     ],
                                             }
                                     ]
-                                }
+                                },
+                                {
+                                    name: "Avatar",
+                                    id: 3,
+                                    description: "Change your twitter profile pic by your identity avatar",
+                                    icon: <svg className={styles.quest_point_icon} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M9.53 16.122a3 3 0 00-5.78 1.128 2.25 2.25 0 01-2.4 2.245 4.5 4.5 0 008.4-2.245c0-.399-.078-.78-.22-1.128zm0 0a15.998 15.998 0 003.388-1.62m-5.043-.025a15.994 15.994 0 011.622-3.395m3.42 3.42a15.995 15.995 0 004.764-4.648l3.876-5.814a1.151 1.151 0 00-1.597-1.597L14.146 6.32a15.996 15.996 0 00-4.649 4.763m3.42 3.42a6.776 6.776 0 00-3.42-3.42" />
+                                    </svg>,
+                                    steps: [6, 4],
+                                    customContent: [
+                                        <p>
+                                            Change your twitter profile pic by your identity avatar. Don't try to cheat, or you won't get the final reward 🤨
+                                        </p>,
+                                    ],
+                                    details: {
+                                        0: {
+                                            buttons: [
+                                                <a href={"https://app.starknet.id/identities/" + datas.identityTokenId + "/"} target="_blank" rel="noreferrer"><button className="button gold">Open Starknet.id</button></a>
+                                            ]
+                                        }
+                                    },
+                                },
                             ]
                     },
                 ]
