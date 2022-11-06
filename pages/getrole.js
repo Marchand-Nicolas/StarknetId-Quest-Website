f9c928d64e7d01b3515a0cb456a804a4ee75ee2d
nicolasito1411 <60229704+Marchand-Nicolas@users.noreply.github.com>
2022-11-06 14:45:14 +0100

    Improving get role page

--- pages/getrole.js
+++ pages/getrole.js
@@ -7,6 +7,7 @@ import callApi from "../utils/callApi";
 import config from "../utils/config";
 import WalletMenu from "../components/walletmenu";
 import popup from "../utils/popup";
+import LoadingScreen from "../components/loadingScreen";
 
 export default function GetRole() {
     const { contract:starknetIdContract } = useStarknetIdContract()
@@ -18,11 +19,11 @@ export default function GetRole() {
     const [ actionUrl, setActionUrl ] = useState('')
     const [ actionDescription, setActionDescription ] = useState('')
     const [ canGetRole, setCanGetRole ] = useState(false)
+    const [ loading, setLoading ] = useState(true)
 
     useEffect(async () => {
         if (!tokenId || !account) return;
         const userDatas = await callApi(`${config.apiUrl}get_nft_datas`, { tokenId: tokenId, player: account })
-        console.log(userDatas)
         setUserDatas(userDatas)
     }, [tokenId, account])
 
@@ -32,6 +33,7 @@ export default function GetRole() {
             fetch(`https://api-testnet.aspect.co/api/v0/assets?owner_address=${account}&contract_address=${contract.address}&sort_by=minted_at&order_by=asc`).then(res => res.json()).then(async res => {
                 const assets = res.assets.map(asset => asset.token_id)
                 if (assets[0]) setTokenId([assets[0], 0])
+                else setLoading(false)
             })
         } catch (error) {
             notify({message:"The Aspect api is currently unavailable. Please check back later.", warning: true})
@@ -49,12 +51,14 @@ export default function GetRole() {
         if (!contract || !starknetIdContract) return;
         const level =  parseInt(await contract.functions.getLevel(tokenId));
         if (level < 7) {
+            setLoading(false)
             setActionDescription("Your NFT must be level 7. Access the Quest Tree to increase its level.")
             setActionName('Quest tree')
             setActionUrl('/quests')
             return;
         }
         const discordUserId = (await starknetIdContract.get_verifier_data([userDatas.identityTokenId, 0], "28263441981469284", "0x06520a4a1934c84a385a3088952c3812c96f9e9c614bc4d483daff5622ea9fad")).toString()
+        setLoading(false)
         if (discordUserId == '0') {
             setActionDescription("You have to link a Discord account to your identity to get the Odyssey OG role on the official Starknet.id server.")
             setActionName("Verify Discord")
@@ -64,29 +68,37 @@ export default function GetRole() {
         setCanGetRole(true)
     }, [starknetIdContract, contract, userDatas, tokenId])
 
-    return account ? canGetRole ? 
-    <div className="popup contener no-anim">
-        <br></br>
-        <center>Your configuration seems correct. You just have to click on the button below to get the role.</center>
-        <br></br>
-        <button onClick={() => callApi(`${config.apiUrl}get_discord_role`, { tokenId: tokenId, player: account }).then(res => {
-            popup("Success", "You have been granted the Odyssey OG role.")
-        })} className="button gold nq-button">
-            <div className="line">
-                <p>Get the role</p>
-            </div>
-        </button>
-    </div>
-    : <div className="popup contener no-anim">
-        <br></br>
-        <center>{actionDescription}</center>
-        <br></br>
-        <a href={actionUrl} target="_blank" rel="noreferrer">
-            <button className="button gold nq-button">
+    return <>
+    {
+        account ? canGetRole ? 
+        <div className="popup contener no-anim">
+            <br></br>
+            <center>Your configuration seems correct. You just have to click on the button below to get the role.</center>
+            <br></br>
+            <button onClick={() => callApi(`${config.apiUrl}get_discord_role`, { tokenId: tokenId, player: account }).then(res => {
+                popup("Success", "You have been granted the Odyssey OG role.")
+            })} className="button gold nq-button">
                 <div className="line">
-                    <p>{actionName}</p>
+                    <p>Get the role</p>
                 </div>
             </button>
-        </a>
-    </div> : <WalletMenu  hasWallet={false} closeWallet={null} url="/getrole" />
+        </div>
+        : <>
+        {
+            loading ? <LoadingScreen /> : <div className="popup contener no-anim">
+            <br></br>
+            <center>{actionDescription}</center>
+            <br></br>
+            <a href={actionUrl} target="_blank" rel="noreferrer">
+                <button className="button gold nq-button">
+                    <div className="line">
+                        <p>{actionName}</p>
+                    </div>
+                </button>
+            </a>
+        </div>
+        }
+    </> : <WalletMenu  hasWallet={false} closeWallet={null} url="/getrole" />
+    }
+    </>
 }
